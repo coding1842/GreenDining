ca2a61c5ee8dd48e40b1d3b1aab63266cfc2c18c
Lee_Gang_Min <dlrkdals227@gmail.com>
2023-10-16 22:18:24 +0900

    #3 - 스프링부트 프로젝트 생성하기
    Feat - 프로젝트 생성 후 저장소에 정상적으로 커밋 하였습니다.

---
+++ src/test/java/com/green/ecom4/GreenDiningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.ecom4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenDiningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
