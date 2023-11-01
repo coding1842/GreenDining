3c19c8fad8507e9bf9b81c93c4c399f041189bbc
sumer <jsy5649@naver.com>
2023-11-01 10:53:04 +0900

    #17 - 일반/사업자 로그인 아이디 및 비밀번호 찾기 웹 디자인

---
+++ src/main/resources/static/js/User.js
@@ -0,0 +1,13 @@
+$(function () {
+  
+   $("#btnradio1").click(function (e) { 
+  	$("#user_find").attr("style","display:block");  
+    $("#business_find").attr("style","display:none");
+  });
+  
+  $("#btnradio2").click(function (e) { 
+  	$("#user_find").attr("style","display:none");  
+    $("#business_find").attr("style","display:block");
+  });
+  
+});
