206870b2043ab95f26bd60bd428a7d6b9be15bb5
coding1842 <coding1842@gmail.com>
2023-11-14 12:34:54 +0900

    Auto stash before merge of "main" and "origin/main"

---
+++ src/main/java/com/ecom4/green/user/dto/AddressDTO.java
@@ -0,0 +1,27 @@
+package com.ecom4.green.user.dto;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@Data
+@Getter
+@Setter
+@Scope(value = WebApplicationContext.SCOPE_SESSION,
+		proxyMode = ScopedProxyMode.TARGET_CLASS)
+@ToString
+@Component
+public class AddressDTO {
+	private String user_id;
+	private String name;
+	private String address;
+	private String address2;
+	private int zipcode;
+	private String request_option;
+	private String request_text;
+}
--- src/main/java/com/ecom4/green/user/service/UserService.java
+++ src/main/java/com/ecom4/green/user/service/UserService.java
@@ -1,5 +1,14 @@
 package com.ecom4.green.user.service;
 
+import java.util.List;
+
+import com.ecom4.green.user.dto.AddressDTO;
+
 public interface UserService
 {
+
+	void insertAddress(AddressDTO addressDTO) throws Exception;
+
+	List<AddressDTO> selectAddressList(String id);
+	
 }
